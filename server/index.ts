cdf216eb894420625f04c6b3a9886542e4270452
jyotirmaygupta23 <jyotirmay.gupta@quadbtech.com>
2025-03-25 21:23:17 +0530

    Deploying to Railway

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
 app.use(express.json());
@@ -58,7 +61,7 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const port = 5000;
+  const port = process.env.PORT;
   server.listen({
     port,
     host: "0.0.0.0",
